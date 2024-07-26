6b34d6c334eb618a089f621ac00780bc61d431ee
Subham <bhandarimaiya65@gmail.com>
2024-07-26 17:41:18 +0545

    routing and response

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return response('<h1>Hello World</h1>', 200)
+        ->header('Content-Type', 'text/html')
+        ->header('X-Custom-Header', 'Hello World');
+});
+
+Route::get('/post/{id}', function ($id) {
+    return response("Post ID: {$id}", 200);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function (Request $request) {
+    dd($request);
+});
+Route::get('/user', function (Request $request) {
+    dd($request->name);
 });
