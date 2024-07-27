d781428f5d4ada97584b0cf7fb479b9165fa52da
Subham <bhandarimaiya65@gmail.com>
2024-07-27 17:05:12 +0545

    blade components and tags component

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,24 @@ Route::get('/', function () {
     ]);
 });
 
-//Single job
-Route::get('/jobs/{id}', function ($id) {
+//Single job by id
+// Route::get('/jobs/{id}', function ($id) {
+//     $jobId = JobListing::find($id);
+
+//     if ($jobId) {
+//         return view('job', [
+//             'job' => JobListing::find($id),
+//         ]);
+//     } else {
+//         abort(404);
+//     }
+
+// });
+
+//Single job by using route model binding in elouquent model
+//we dont have to put abort 404 if job not found it will automatically do that
+Route::get('/jobs/{jobListing}', function (JobListing $jobListing) {
     return view('job', [
-        'job' => JobListing::find($id),
+        'job' => $jobListing,
     ]);
 });
