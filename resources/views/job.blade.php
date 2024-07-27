fcb441465ea6722a74930c797072522c00e5b550
Subham <bhandarimaiya65@gmail.com>
2024-07-27 13:30:18 +0545

    main layout and home page

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -1,3 +1,7 @@
-<h1>Job id: {{ $job['id'] }}</h1>
-<h2>Job title: {{ $job['title'] }}</h2>
-<p>Job description: {{ $job['description'] }}</p>
+@extends('layout')
+
+@section('content')
+    <h1>Job id: {{ $job['id'] }}</h1>
+    <h2>Job title: {{ $job['title'] }}</h2>
+    <p>Job description: {{ $job['description'] }}</p>
+@endsection
