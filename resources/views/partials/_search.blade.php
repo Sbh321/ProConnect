52411bb0cef926d0ec484c35df87ffe3f3b6e51e
subham <bhandarimaiya65@gmail.com>
2025-08-15 05:46:17 +0545

    feat: change jobs to gigs

--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -4,14 +4,14 @@
             <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
         </div>
         <input type="text" name="search" class="h-14 w-full pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
-            placeholder="Search Laravel Gigs..." />
+            placeholder="Search for Gigs..." />
         <div class="absolute top-2 right-2">
             <button type="submit" class="h-10 w-20 text-white rounded-lg bg-blue-500 hover:bg-blue-600">
                 Search
             </button>
             <a href="/jobs/create"
                 class="rounded-lg bg-black text-white py-2 px-5 hover:bg-white hover:text-black border-2 hover:border-black border-blueGray transition-all">Post
-                Job</a>
+                Gig</a>
         </div>
     </div>
 </form>
